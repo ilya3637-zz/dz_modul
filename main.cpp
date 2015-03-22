b9b35a2fd427d68fc27ae70cc789c0862da91f9b
Илья <ilya3637@mail.ru>
2015-03-22 23:31:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 #include "Street.h"
+#include "House.h"
 
 using namespace std;
 
-
-void replace(){}
-
-
-void unionList(){}
-
 int main()
 {
-	setlocale(0,"");
-	
-	
+
+	setlocale(0, "");
+	Street street("newstreet", true, true);
+	int n = 0;
+
+	int number, countOfFlats;
+	bool newHouse, inRepair;
+	double rent;
+
+	cout<<"Введите количество домов"<<endl;
+	cin>>n;
+	for (int i = 0; i < n; i++) {
+
+		cout<<"номер дома "<<endl;
+		cin >> number;
+		cout<<"новый? "<<endl;
+		cin >> newHouse;
+		cout << "на ремонте? "<<endl;
+		cin >> inRepair;
+		cout << "количество квартир "<<endl;
+		cin >> countOfFlats;
+		cout << "квартплата "<<endl;
+		cin >> rent;
+		House house(number,newHouse,inRepair,countOfFlats,rent);
+		street.add(house);
+
+	}
+	system("pause");
 	return 0;
 }
+
+
